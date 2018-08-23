ab5112e8f4aa67a8e37de6f4e4f92718285c5185
MBOng124 <33584389+okLang32@users.noreply.github.com>
2018-08-24 00:00:29 +0800

    Added states

--- src/model/Node.java
+++ src/model/Node.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class Node {
     private ArrayList<Integer> data;
-    private Node next;
+    private ArrayList<Node> next;
+    private ArrayList<Node> prev;
 
     public Node(ArrayList<Integer> data){
         this.data = data;
     }
 
-    public Node(ArrayList<Integer> data, Node next){
-        this.data = data;
-        this.next = next;
+    public Node(){
+
     }
 
     public ArrayList<Integer> getData() {
@@ -23,11 +23,27 @@ public class Node {
         this.data = data;
     }
 
-    public Node getNext() {
+    public ArrayList<Node> getNext() {
         return next;
     }
 
-    public void setNext(Node next) {
+    public void setNext(ArrayList<Node> next) {
         this.next = next;
+    }
+
+    public void addNext(Node node){
+        this.next.add(node);
+    }
+
+    public ArrayList<Node> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(ArrayList<Node> prev) {
+        this.prev = prev;
+    }
+
+    public void addPrev(Node previous){
+        this.prev.add(previous);
     }
 }
